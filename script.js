c7635bee222cfa06086d3d4c87cd8e58f536d8ee
MakeithappenMike <omichaelaaron@gmail.com>
2022-08-06 15:33:28 -0400

    got cities updating in localStorage array

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Define things
 var url = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=33.44&exclude=hourly,daily&appid=6798ccba44792929ff2f3dacdfb753cd"
 var searchHistory = [];
+console.log("exsitingHistory: ", existingHistory);
 
 // Load weather from API
 fetch(url)
@@ -9,14 +10,17 @@ fetch(url)
   console.log(url);
 
 // Show recent searches in search history
+
 if (localStorage.getItem("searchHistory") != null) {
-    // searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
+    var existingHistory = JSON.parse(localStorage.getItem("searchHistory"));
     console.log("SearchHistory:", searchHistory);
+    searchHistory = [...existingHistory];
 };
 $(".searchButton").click(function () {
     var city = $(".input").val();
     searchHistory.push(city);
     console.log(city);
     console.log("click");
-    // localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+    city = "";
 });
