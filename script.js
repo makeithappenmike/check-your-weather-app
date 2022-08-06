917c69d071ca95e9c15d33371dc77998a27c3f02
MakeithappenMike <omichaelaaron@gmail.com>
2022-08-06 15:24:52 -0400

    working on gettin search history going

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+// Define things
 var url = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=33.44&exclude=hourly,daily&appid=6798ccba44792929ff2f3dacdfb753cd"
+var searchHistory = [];
+
+// Load weather from API
 fetch(url)
   .then((response) => response.json())
   .then((data) => console.log(data));
   console.log(url);
+
+// Show recent searches in search history
+if (localStorage.getItem("searchHistory") != null) {
+    // searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
+    console.log("SearchHistory:", searchHistory);
+};
+$(".searchButton").click(function () {
+    var city = $(".input").val();
+    searchHistory.push(city);
+    console.log(city);
+    console.log("click");
+    // localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+});
